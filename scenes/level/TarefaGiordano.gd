37ba419afaacefbae37ce7d169d824fa3230df19
Guilherme <gui.crb0@gmail.com>
2022-02-16 15:11:47 -0300

    3 tracks

--- scenes/level/TarefaGiordano.gd
+++ scenes/level/TarefaGiordano.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
 onready var placeholderTrack = $PlaceholderTrack
-onready var track1 = $Pos0/Track1
-onready var track2 = $Pos1/Track2
+onready var track0 = $Pos0/Track0
+onready var track1 = $Pos1/Track1
+onready var track2 = $Pos2/Track2
 
 func _ready():
 	$PlaceholderTrack.play()
@@ -10,11 +11,19 @@ func _ready():
 func _process(delta):
 	$Player.play()
 	if Input.is_action_just_pressed("pos0"):
-		print(1)
 		$Player.position = $Pos0/Pos.global_position
 	elif Input.is_action_just_pressed("pos1"):
-		print(2)
 		$Player.position = $Pos1/Pos.global_position
+	elif Input.is_action_just_pressed("pos2"):
+		$Player.position = $Pos2/Pos.global_position
+
+func _on_AddTrack0_body_entered(body):
+	if not track0.playing:
+		var pos = placeholderTrack.get_playback_position()
+		track0.play(pos)
+
+func _on_RemoveTrack0_body_entered(body):
+	track0.stop()
 
 func _on_AddTrack1_body_entered(body):
 	if not track1.playing:
@@ -23,7 +32,7 @@ func _on_AddTrack1_body_entered(body):
 
 func _on_RemoveTrack1_body_entered(body):
 	track1.stop()
-
+	
 func _on_AddTrack2_body_entered(body):
 	if not track2.playing:
 		var pos = placeholderTrack.get_playback_position()
