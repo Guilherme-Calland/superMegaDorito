5080c84103e1a9adeaacf14ed92ec4af7e293e29
Guilherme <gui.crb0@gmail.com>
2022-02-24 15:34:10 -0300

    camera like celeste complete

--- scripts/level/TestLevel.gd
+++ scripts/level/TestLevel.gd
@@ -13,17 +13,18 @@ func _on_MovementLock_body_entered(body, lockDirection):
 		$Player.rightLock = true
 
 func changeCameraPosition(newPosition, lock):
-	$Camera2D.changePosition(newPosition)
+	$CameraNodes/Camera2D.changePosition(newPosition)
 	if lock == 1:
-		$MovementLock1/CollisionShape2D.set_deferred("disabled", false)
+		$CameraNodes/MovementLock1/CollisionShape2D.set_deferred("disabled", false)
 	elif lock == 3:
-		$MovementLock3/CollisionShape2D.set_deferred("disabled", false)
+		$CameraNodes/MovementLock3/CollisionShape2D.set_deferred("disabled", false)
 
 func connectCameraTriggers():
-	$MoveCameraTrigger1.connect("cameraTrigger", self, "changeCameraPosition")
-	$MoveCameraTrigger2.connect("cameraTrigger", self, "changeCameraPosition")
-	$MoveCameraTrigger3.connect("cameraTrigger", self, "changeCameraPosition")
+	$CameraNodes/MoveCameraTrigger1.connect("cameraTrigger", self, "changeCameraPosition")
+	$CameraNodes/MoveCameraTrigger2.connect("cameraTrigger", self, "changeCameraPosition")
+	$CameraNodes/MoveCameraTrigger3.connect("cameraTrigger", self, "changeCameraPosition")
+	$CameraNodes/MoveCameraTrigger4.connect("cameraTrigger", self, "changeCameraPosition")
 
 func _on_MovementLock_body_exited(body):
-		$Player.leftLock = false
-		$Player.rightLock = false
+	$Player.leftLock = false
+	$Player.rightLock = false
